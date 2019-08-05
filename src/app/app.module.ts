a14082f730dbe1aa583898f85baee4cf6094ab45
shra012 <shravan_fisher@yahoo.ca>
2019-08-05 22:46:57 +0530

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { UserSettingsFormComponent } from './user-settings-form/user-settings-form/user-settings-form.component';
+import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
